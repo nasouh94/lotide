6f7b5549c47f546bd22263fdaa38f773af4dd41a
nasouh94 <nasouh@live.com>
2019-09-17 21:49:17 +0000

    assertArraysEqual works with console.log testing

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(array1, array2)
+{
+  if(array1.length === array2.length)
+  {
+    for (var i = 0; i < array1.length; i ++)
+    {
+      if(array1[i] !== array2[i])
+      {
+        //console.log("Arrays are not the same");
+        return false;
+      }
+    }
+
+  }
+  else
+  {
+    //console.log("arrays not same length, therefore not the same");
+    return false;
+  }
+//console.log("arrays are the same!");
+return true;
+
+
+}
+
+// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) prints falase
+// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]); // prints true
