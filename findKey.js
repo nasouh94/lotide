f2fabe68021d749611b59038e927d77bea4c8753
nasouh94 <nasouh@live.com>
2019-09-19 19:31:26 +0000

    works with assertEqual as well

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:, ${actual.toString()}  ===  ${expected.toString()}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: , ${actual.toString()}  !==  ${expected.toString()}`);
+  
+  }
+};
+
+
+let findKeyIn = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+let findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+let found = findKey(findKeyIn, (item) => item.stars === 2);
+console.log(found);
+
+assertEqual(found, "noma");
+
